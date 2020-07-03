d1e79b2f82608a60a4537971730794d8db2623ce
Sur Sood <soodsur007@gmail.com>
2020-07-03 14:57:42 -0400

    heroku

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 # project2_env
 
 from flask import Flask, render_template
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
