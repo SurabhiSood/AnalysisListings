9f05eff9dc632abd81a5f0c0abdcff43da6b1a4c
Sur Sood <soodsur007@gmail.com>
2020-07-08 11:02:17 -0400

    rabba ji

--- app.py
+++ app.py
@@ -1,18 +1,38 @@
 # project2_env
 
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template,jsonify
+import os import environ
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+# Create engine
+# conn_str = 'postgres://yoyqxloquekucy:8328f25bf2390670b4035437fdf6329cfcd035d476d4bfc89e49f128471ee4f9@ec2-54-165-36-134.compute-1.amazonaws.com:5432/daafhqmf1lebno'
+# engine = create_engine(conn_str)
+
+# Reflect the database 
+Base = automap_base()
+
+# Reflect the tables into SQLAlchemy classes
+Base.prepare(engine, reflect=True)
+allclasses = Base.classes.keys()
 
 @app.route('/')
 def map_ratings():
     return render_template('map_reviews.html')
 
+@app.route("/test")
+def text():
+
+    return f"All classes available in the database: {allclasses}"
+    
 if __name__== '__main__':
     app.run(debug=True)
--- static/js/mapLogic.js
+++ static/js/mapLogic.js
@@ -62,3 +62,4 @@ function init(){
   });
 };
 init()
+
