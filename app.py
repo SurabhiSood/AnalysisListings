87a7da5be3127f2c2b30f7d5d78d8ab5587d3fac
Sur Sood <soodsur007@gmail.com>
2020-07-08 11:13:18 -0400

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # project2_env
 
 from flask import Flask, render_template,jsonify
-import os import environ
+import os
+from os import environ
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
@@ -9,20 +10,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
-
-# Create engine
-
-# engine = create_engine(conn_str)
 
 # Reflect the database 
 Base = automap_base()
 
 # Reflect the tables into SQLAlchemy classes
-Base.prepare(engine, reflect=True)
 allclasses = Base.classes.keys()
 
 @app.route('/')
